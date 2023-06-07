d97faa6a454783df64cfeaa74c8ecdcc3bbe5b36
anıl <nbr-nb@hotmail.com>
2023-06-07 19:10:28 +0300

    Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] TempArray = items;
+            items = new T[items.Length + 1];
+
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                items[i] = TempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
