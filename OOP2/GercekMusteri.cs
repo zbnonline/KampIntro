3d5c9b30ca9286878a76c7dc9108f6d6071d830e
anıl <nbr-nb@hotmail.com>
2023-06-08 19:28:51 +0300

    OOP2 klasöründe Inheritance konusu işlendi.

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class GercekMusteri: Musteri
+    {
+        public string TcNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ömer Zabunoğlu
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.FirstName = "Ömer";
+            musteri1.LastName = "Zabunoğlu";
+            musteri1.MusteriNo = "6161616161";
+            musteri1.TcNo = "12345678910";
+
+            // Weonsoft
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.SirketAdi = "Weonsoft";
+            musteri2.VergiNo = "61616161";
+            musteri2.MusteriNo = "1357902468";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+        }
+    }
+}
