ef82e0705139cdcc871fc5f8a902cf132b0a22ae
anıl <nbr-nb@hotmail.com>
2023-06-10 21:48:57 +0300

    Interface konusu işlendi

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        public void BavuruYap(IKrediManager krediManager, List<ILoggerService> loggerServices)
+        {
+            krediManager.Hesapla();
+
+            foreach (var logger in loggerServices)
+            {
+                logger.Log();
+            }
+            
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            List<ILoggerService> loggerServices = new List<ILoggerService>() { fileLoggerService, databaseLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BavuruYap(ihtiyacKrediManager, loggerServices);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
